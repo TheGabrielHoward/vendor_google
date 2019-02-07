21785e4ee0b7fe40e270df93969afc5279a67479
shagbag913 <sh4gbag913@gmail.com>
2019-02-06 23:58:28 -0500

    marlin/sailfish: regen after apv changes
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- marlin/BoardConfigVendorPartial.mk
+++ marlin/BoardConfigVendorPartial.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2019 BootleggersROM
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 TARGET_BOARD_INFO_FILE := vendor/google_devices/marlin/vendor-board-info.txt
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
--- marlin/device-vendor-sailfish.mk
+++ marlin/device-vendor-sailfish.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2019 BootleggersROM
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 $(call inherit-product, vendor/google_devices/sailfish/sailfish-vendor-blobs.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -21,7 +35,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.storage_manager.enabled=true \
     ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
 
-PRODUCT_PACKAGE_OVERLAYS += vendor_overlay/google_devices/sailfish/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/google_devices/sailfish/overlay
 
 # Prebuilt APKs/JARs from 'vendor/app'
 PRODUCT_PACKAGES += \
